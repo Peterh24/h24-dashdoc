171f0518571dcf65c55b33bfe7ea86beef1f248e
Sylvain Bousselier <orphen92300@gmail.com>
2023-05-24 15:42:29 +0200

    start

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'h24-transports-ionic',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
