d6cef5c35264435862e46f549d4a44b645300f9f
Sylvain Bousselier <bousselier@natexo.com>
2023-06-02 12:35:12 +0200

    Adding Interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,38 @@
 import { IonicStorageModule } from '@ionic/storage-angular';
-import { NgModule } from '@angular/core';
+import { NgModule, inject } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpInterceptorFn, provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { Drivers } from '@ionic/storage';
+import { Storage } from '@ionic/storage-angular';
 import * as cordovaSQLiteDriver from 'localforage-cordovasqlitedriver';
+import { from, switchMap } from 'rxjs';
+import { USER_STORAGE_KEY } from './services/constants';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const storage = inject(Storage);
+  const tokenObs = from(storage.get(USER_STORAGE_KEY));
+  console.log('req: ', req.url )
+
+  return tokenObs.pipe(
+    switchMap((token) => {
+      console.log('Token in Interceptor: ', token)
+      if(token) {
+        req = req.clone({
+          setHeaders: {
+            Authorization: `Token ${token}`
+          }
+        });
+      }
+      return next(req);
+    })
+  )
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -27,7 +50,7 @@ import * as cordovaSQLiteDriver from 'localforage-cordovasqlitedriver';
   ],
 
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideHttpClient()
+    provideHttpClient(withInterceptors([authInterceptor]))
   ],
   bootstrap: [AppComponent],
 })
--- src/app/private/profile/details/details.page.ts
+++ src/app/private/profile/details/details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DASHDOC_API_URL } from '../../../../app/services/constants';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
+    this.http.get(`${DASHDOC_API_URL}/adr-un-codes/`).pipe(take(1)).subscribe((res) => {
+      console.log('res: ', res);
+    })
   }
 
 }
