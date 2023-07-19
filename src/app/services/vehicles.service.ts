94d6fc7cea0b1301f0bfe233a3310117e4ddb7c7
Sylvain Bousselier <bousselier@daskmail.com>
2023-07-19 15:02:44 +0200

    fix camion

--- src/app/services/vehicles.service.ts
+++ src/app/services/vehicles.service.ts
@@ -10,7 +10,7 @@ export class VehiclesService {
     new Vehicle('3M3', 'Man', '3m3', 6.8, 4, 2.6, 3.8, 500, 80),
     new Vehicle('8M3', 'Man', '8m3', 6.8, 4, 2.6, 3.8, 500, 80),
     new Vehicle('14M3', 'Man', '14m3', 6.8, 4, 2.6, 3.8, 500, 80),
-    new Vehicle('12M3HAYON', 'Man', '14m3', 6.8, 4, 2.6, 3.8, 500, 80),
+    new Vehicle('14M3HAYON', 'Man', '14m3', 6.8, 4, 2.6, 3.8, 500, 80),
     new Vehicle('20M3', 'Man', '20m3', 6.8, 4, 2.6, 3.8, 500, 80),
     new Vehicle('20M3HAYON', 'Man', '20m3', 6.8, 4, 2.6, 3.8, 500, 80)
   ])
