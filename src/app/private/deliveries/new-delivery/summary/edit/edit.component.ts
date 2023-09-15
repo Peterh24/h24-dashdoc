a43dc2fd80773c193ff5af234709091629fad862
Sylvain Bousselier <bousselier@daskmail.com>
2023-09-15 17:11:06 +0200

    edit merchandise

--- src/app/private/deliveries/new-delivery/summary/edit/edit.component.ts
+++ src/app/private/deliveries/new-delivery/summary/edit/edit.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, ViewChild } from '@angular/core';
 import { IonDatetime, ModalController } from '@ionic/angular';
 import { addHours, format, subHours } from 'date-fns';
 import { TransportService } from 'src/app/services/transport.service';
+import { Merchandise } from '../../../delivery.model';
+import { ModalQuantityComponent } from '../../merchandise/modal-quantity/modal-quantity.component';
 
 @Component({
   selector: 'app-edit',
@@ -17,6 +19,43 @@ export class EditComponent {
   defaultValue:any;
   minVal: any;
   maxVal: any;
+  merchandises: Array<Merchandise> = [
+    {
+      id: 'camera',
+      name: 'Caméra',
+      quantity: 0
+    },
+    {
+      id: 'light',
+      name: 'lumières',
+      quantity: 0
+    },
+    {
+      id: 'photo',
+      name: 'Photographie',
+      quantity: 0
+    },
+    {
+      id: 'management',
+      name: 'Régie',
+      quantity: 0
+    },
+    {
+      id: 'clothe',
+      name: 'Vêtements',
+      quantity: 0
+    },
+    {
+      id: 'machinery',
+      name: 'Machinerie',
+      quantity: 0
+    },
+    {
+      id: 'furniture',
+      name: 'Mobilier / Décor',
+      quantity: 0
+    }
+  ];
   constructor(
     private modalController: ModalController,
     private transportService: TransportService,
@@ -45,6 +84,13 @@ export class EditComponent {
         }
       })
     }
+
+    this.dataToEdit.planned_loads.forEach((plannedLoad:any) => {
+      const merchandise = this.merchandises.find(item => item.id === plannedLoad.id);
+      if (merchandise) {
+        merchandise.quantity = plannedLoad.quantity;
+      }
+    })
     console.log(this.dataToEdit);
   }
 
@@ -65,6 +111,66 @@ export class EditComponent {
       this.dataToEdit.destination.slots[0].end = value;
     }
     this.defaultValue = this.minVal;
+  }
+
+  async onMerchandiseClick(itemId: string, itemName: string, quantity: number) {
+    const modal = await this.modalController.create({
+      component: ModalQuantityComponent,
+      componentProps: {
+        id: itemId,
+        name: itemName,
+        quantity: quantity
+      },
+      cssClass: 'quantity-modal',
+      mode: 'ios'
+    });
+
+    modal.present();
+    const { data } = await modal.onWillDismiss();
+
+    if (data) {
+      const index = this.merchandises.findIndex(merchandise => merchandise.id === data.id);
+
+      // Mise à jour dans la liste de merchandises
+      if (index !== -1) {
+        this.merchandises[index].quantity = data.quantity;
+        this.merchandises[index].name = data.name;
+      } else {
+        this.merchandises.push({
+          id: data.id,
+          name: data.name,
+          quantity: data.quantity
+        });
+      }
+
+      // Mise à jour dans la liste des planned_loads
+      const lastIndex = this.transportService.deliveries.length - 1;
+      const plannedLoads = this.transportService.deliveries[lastIndex].planned_loads;
+
+      const existingIndex = plannedLoads.findIndex((item:any) => item.id === data.id);
+
+      if (existingIndex !== -1) {
+        plannedLoads[existingIndex] = {
+          id: data.id,
+          name: data.name,
+          quantity: data.quantity
+        };
+      } else {
+        plannedLoads.push({
+          id: data.id,
+          name: data.name,
+          quantity: data.quantity
+        });
+      }
+
+    }
+  }
+
+  getImage(image: string){
+    if(image.includes('other')){
+      return 'other';
+    }
+    return image;
   }
 
   cancel() {
