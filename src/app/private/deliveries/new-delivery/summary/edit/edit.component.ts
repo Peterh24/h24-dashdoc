97e3423aa96ecc75ca478b4b8c0b9867516c2c6a
orphen92 <orphen92300@gmail.com>
2023-09-15 23:35:10 +0200

    maybe fixing date

--- src/app/private/deliveries/new-delivery/summary/edit/edit.component.ts
+++ src/app/private/deliveries/new-delivery/summary/edit/edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, ViewChild } from '@angular/core';
 import { IonDatetime, ModalController } from '@ionic/angular';
-import { addHours, format, subHours } from 'date-fns';
+import { addHours, format, getHours, getMinutes, setHours, setMinutes, subHours } from 'date-fns';
 import { TransportService } from 'src/app/services/transport.service';
 import { Merchandise } from '../../../delivery.model';
 import { ModalQuantityComponent } from '../../merchandise/modal-quantity/modal-quantity.component';
@@ -63,25 +63,36 @@ export class EditComponent {
 
   ionViewWillEnter() {
     if(this.modalType === 'date-origin'){
+      let maxDestinationDate: any;
       this.defaultValue = this.dataToEdit.origin.slots[0].start;
+      this.minVal = format(new Date(), 'yyyy-MM-dd');
 
+
+
+      // Set the max val
+      this.transportService.deliveries.forEach(delivery => {
+        const destinationStartDate = format(new Date(delivery.destination.slots[0].start), 'yyyy-MM-dd\'T\'HH:mm');
+        if (!maxDestinationDate || destinationStartDate > maxDestinationDate) {
+          maxDestinationDate = destinationStartDate;
+        }
+      });
+      console.log('maxDestinationDate: ', maxDestinationDate);
+      this.maxVal = format(new Date(maxDestinationDate), 'yyyy-MM-dd');
+
+      // If date is equal to current date min val is Current date + 1 hour
       if(format(new Date(), 'yyyy-MM-dd') === format(new Date(this.defaultValue), 'yyyy-MM-dd')){
         this.minVal = format(addHours(new Date(), 1), 'yyyy-MM-dd\'T\'HH:mm');
-      } else {
-        this.minVal = format(new Date(2000, 0, 1), 'yyyy-MM-dd\'T\'HH:mm');
       }
-
-
-      this.maxVal = format(subHours(new Date(this.dataToEdit.destination.slots[0].start), 1), 'yyyy-MM-dd\'T\'HH:mm');
-    } else if(this.modalType === 'date-destination'){
+    } else {
+      let maxOriginDate: any;
       this.defaultValue = this.dataToEdit.destination.slots[0].start;
-      let maxDeliveryDate: any;
       this.transportService.deliveries.forEach(delivery => {
         const originStartDate = format(new Date(delivery.origin.slots[0].start), 'yyyy-MM-dd\'T\'HH:mm');
-        if (!maxDeliveryDate || originStartDate > maxDeliveryDate) {
-          maxDeliveryDate = originStartDate;
-          this.minVal = originStartDate;
+        if (!maxOriginDate || originStartDate > maxOriginDate) {
+          maxOriginDate = originStartDate;
         }
+        this.minVal = format(addHours(new Date(maxOriginDate), 1), 'yyyy-MM-dd\'T\'HH:mm');
+        
       })
     }
 
@@ -97,9 +108,43 @@ export class EditComponent {
   toggleDate(){
     if(this.type === 'date'){
       this.type = 'time';
+
+      if(this.modalType === 'date-destination'){
+        const storedDate = this.dataToEdit.destination.slots[0].start;
+        let maxOriginDate: any;
+        this.transportService.deliveries.forEach(delivery => {
+          const originStartDate = format(new Date(delivery.origin.slots[0].start), 'yyyy-MM-dd\'T\'HH:mm');
+          if (!maxOriginDate || originStartDate > maxOriginDate) {
+            maxOriginDate = originStartDate;
+            const isSameday: boolean = format(new Date(storedDate), 'yyyy-MM-dd') === format(new Date(maxOriginDate), 'yyyy-MM-dd');
+            if(!isSameday){
+              this.minVal = format(new Date(2000, 0, 1), 'yyyy-MM-dd\'T\'HH:mm')             
+            }
+          }
+        })
+      }
+
     } else {
       this.type = 'date';
+
+      if(this.modalType === 'date-destination'){
+        let maxOriginDate: any;
+        this.defaultValue = this.dataToEdit.destination.slots[0].start;
+        this.transportService.deliveries.forEach(delivery => {
+          const originStartDate = format(new Date(delivery.origin.slots[0].start), 'yyyy-MM-dd\'T\'HH:mm');
+          if (!maxOriginDate || originStartDate > maxOriginDate) {
+            maxOriginDate = originStartDate;
+          }
+          this.minVal = format(addHours(new Date(maxOriginDate), 1), 'yyyy-MM-dd\'T\'HH:mm');
+          
+        })
+      }
+
     }
+
+
+
+    
   }
 
   onChange(value:any){
@@ -110,7 +155,8 @@ export class EditComponent {
       this.dataToEdit.destination.slots[0].start = value;
       this.dataToEdit.destination.slots[0].end = value;
     }
-    this.defaultValue = this.minVal;
+    console.log()
+    this.defaultValue = value;
   }
 
   async onMerchandiseClick(itemId: string, itemName: string, quantity: number) {
