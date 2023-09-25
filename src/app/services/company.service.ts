8178371d1e7be44ae8b3241654960ef02d209a4e
orphen92 <orphen92300@gmail.com>
2023-09-25 16:37:27 +0200

    Add society list

--- src/app/services/company.service.ts
+++ src/app/services/company.service.ts
@@ -5,6 +5,7 @@ import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http
 import { DASHDOC_API_URL, USER_STORAGE_KEY } from './constants';
 import { Storage } from '@ionic/storage-angular';
 import { DashdocService } from './dashdoc.service';
+import { AlertController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -27,7 +28,8 @@ export class CompanyService {
   constructor(
     private http: HttpClient,
     private storage: Storage,
-    private dashdocService: DashdocService
+    private dashdocService: DashdocService,
+    private alertController: AlertController,
   ) { }
 
   fetchCompanies() {
@@ -44,46 +46,50 @@ export class CompanyService {
           try {
             const resData: any = await firstValueFrom(this.http.get(`${DASHDOC_API_URL}addresses/`, { headers }));
             
-            // Traitement des données si la requête réussit
+            // request success
             const newCompany = { ...resData.results[0].created_by, token: token.token };
-            console.log('newCompany: ', newCompany);
+            if (!this._companies.getValue().includes(newCompany)) {
+              this._companies.next([...this._companies.getValue(), newCompany]);
+            }
           } catch (error) {
             if (error instanceof HttpErrorResponse) {
               if (error.status === 401) {
-                // Traitement spécifique pour l'erreur 401 (Unauthorized)
-                alert('votre token ' + token.token+ ' est incorect merci de le verifier dans votre interface dashdoc')
-                // Vous pouvez choisir d'ajouter ce token à une liste de tokens invalides, par exemple
+                const alert = await this.alertController.create({
+                  header: 'Erreur',
+                  message: 'votre token <b>' + token.token+ '</b> est incorect merci de le verifier dans votre interface dashdoc',
+                  buttons: ['Compris'],
+                });
+        
+                await alert.present();
+                return;
               } else {
-                // Gérer d'autres erreurs HTTP si nécessaire
-                console.error('Erreur HTTP:', error.status, error.statusText);
+                // Other Http error
+                console.error('Erreur inattendue:', error);
+                const alert = await this.alertController.create({
+                  header: 'Erreur',
+                  message: 'Votre requete n\'a pas aboutie merci de reesayer ulterieurement',
+                  buttons: ['Compris'],
+                });
+        
+                await alert.present();
+                return;
               }
             } else {
-              // Gérer d'autres types d'erreurs si nécessaire
+              // Other error
               console.error('Erreur inattendue:', error);
+              const alert = await this.alertController.create({
+                header: 'Erreur',
+                message: 'Votre requete n\'a pas aboutie merci de reesayer ulterieurement',
+                buttons: ['Compris'],
+              });
+      
+              await alert.present();
+              return;
             }
           }
         }
       })
     });
-    // this.dashdocService.fetchTokens().pipe(take(1)).subscribe((tokens) => {
-    //   console
-    //   this.dashdocService.tokens.pipe(take(1)).subscribe(async tokens => {
-    //     if (this._companies.getValue().length > 0) {
-    //       return;
-    //     }
-    //     for (const token of tokens) {
-    //       const tokenCurrent = token.token;
-  
-    //       const headers = new HttpHeaders().set('Authorization', `Token ${tokenCurrent}`);
-    //       const resData: any = await firstValueFrom(this.http.get(`${DASHDOC_API_URL}addresses/`, { headers }));
-    //       const newCompany = { ...resData.results[0].created_by, token: token.token };
-  
-    //       if (!this._companies.getValue().includes(newCompany)) {
-    //         this._companies.next([...this._companies.getValue(), newCompany]);
-    //       }
-    //     }
-    //   });
-    // });
   }
 
   getCompany(pk: number) {
