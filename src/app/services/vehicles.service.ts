e2c3b0700472f5afda509ff43f6ed48943054761
orphen92 <orphen92300@gmail.com>
2023-10-02 01:18:31 +0200

    Remove comment

--- src/app/services/vehicles.service.ts
+++ src/app/services/vehicles.service.ts
@@ -8,14 +8,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class VehiclesService {
-  private _vehicles = new BehaviorSubject<Array<Vehicle>>([
-    // new Vehicle('3M3', 'Man', '3m3', 6.8, 4, 2.6, 3.8, 500, 80, false),
-    // new Vehicle('8M3', 'Man', '8m3', 6.8, 4, 2.6, 3.8, 500, 80, false),
-    // new Vehicle('14M3', 'Man', '14m3', 6.8, 4, 2.6, 3.8, 500, 80, false),
-    // new Vehicle('14M3HAYON', 'Man', '14m3', 6.8, 4, 2.6, 3.8, 500, 80, true),
-    // new Vehicle('20M3', 'Man', '20m3', 6.8, 4, 2.6, 3.8, 500, 80, false),
-    // new Vehicle('20M3HAYON', 'Man', '20m3', 6.8, 4, 2.6, 3.8, 500, 80, true)
-  ])
+  private _vehicles = new BehaviorSubject<Array<Vehicle>>([]);
 
   get vehicles() {
     return this._vehicles.asObservable();
