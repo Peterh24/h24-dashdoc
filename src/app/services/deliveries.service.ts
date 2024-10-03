b9a69002a6e131f5ea3999455bdab774957950fd
david <david.ramboz@gmail.com>
2024-10-03 15:41:51 +0200

    slot bug

--- src/app/services/deliveries.service.ts
+++ src/app/services/deliveries.service.ts
@@ -101,15 +101,13 @@ export class DeliveriesService {
     let data;
 
     if (source === 'origin') {
-      if (delivery[0]?.origin?.slots[0]?.start) {
+      if (delivery[0]?.origin?.slots?.[0]?.start) {
         const startDate = new Date(delivery[0].origin.slots[0].start);
         const formattedStartDate = format(startDate, 'dd-MM-yyyy');
         data = `${formattedStartDate} - ${delivery[0].origin.address.postcode}`;
       }
     } else if (source === 'destination') {
-      if (  delivery[0]?.destination &&
-        delivery[0].destination.slots &&
-        delivery[0].destination.slots[0]?.end) {
+      if (delivery[0].destination?.slots?.[0]?.end) {
         const endDate = new Date(delivery[delivery.length - 1].destination.slots[0].end);
         const formattedEndDate = format(endDate, 'dd-MM-yyyy');
         data = `${formattedEndDate} - ${delivery[delivery.length - 1].destination.address.postcode}`;
