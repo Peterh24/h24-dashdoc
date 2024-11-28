712515f70fcbe246b86cf15cf198adb858dfc614
david <david.ramboz@gmail.com>
2024-11-28 11:46:32 +0100

    icône application sur fond noir

---
+++ android-base/app/src/main/java/io/h24transports/MainActivity.java
@@ -0,0 +1,56 @@
+package io.h24transports;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onResume() {
+      super.onResume();
+      WebView webView = getBridge().getWebView();
+      WebSettings webSettings = webView.getSettings();
+      webSettings.setLoadWithOverviewMode(true);
+      webSettings.setUseWideViewPort(true);
+    }
+
+    @Override
+    public void onNewIntent (Intent intent) {
+      super.onNewIntent(intent);
+
+      String action = intent.getAction();
+      Uri data = intent.getData();
+
+      if (data != null) {
+        String url = data.toString();
+        if (url != null) {
+          if (url.contains("https://h24transports.com")) {
+            WebView webView = getBridge().getWebView();
+
+            String absoluteUrl = url.replace("https://h24transports.com", "");
+            /*
+            url = webView.getUrl().replaceFirst("^(https?://[^/]+).*$", "$1" + absoluteUrl);
+             */
+            webView.loadUrl("https://localhost" + absoluteUrl);
+          }
+        }
+      }
+    }
+
+  @Override
+  public boolean onKeyDown(int keyCode, KeyEvent event) {
+    WebView webView = getBridge().getWebView();
+
+    if (keyCode == KeyEvent.KEYCODE_BACK) {
+      Log.w("BACK_BUTTON1", webView.getUrl());
+    }
+
+    return super.onKeyDown(keyCode, event);
+  }
+}
