15976116121e91bd8d6bee45e3d865672999e9c0
Peter Saint-edward <petersaint-edward@Mac.lan>
2025-02-05 01:41:07 +0100

    add server.js to prod

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-var gzippo = require('gzippo');
-var express = require('express');
-var morgan = require('morgan');
-var app = express();
+//Install express server
+const express = require('express');
+const path = require('path');
 
-app.use(morgan('prod'));
-app.use(gzippo.staticGzip("" + __dirname + "/www"));
-app.listen(process.env.PORT || 5000); 
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
